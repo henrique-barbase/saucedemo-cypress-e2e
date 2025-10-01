046d11e2c6271e50ae7d30f1cf86f48bb2f15eab
Henrique de Sousa Barbase <henrique2029@hotmail.com>
2025-10-01 15:43:16 -0300

    Adicionando relatório dos testes

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,16 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://www.saucedemo.com/",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Aqui você pode registrar plugins se precisar
     },
   },
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: false,
+    json: true
+  }
 });
